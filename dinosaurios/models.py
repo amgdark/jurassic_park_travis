3fe4d1e1b64af42bf024f8f759e53d5dbd36e1f2
Alex Mauricio <amgdark@uaz.edu.mx>
2019-10-21 14:42:52 -0500

    Agrega lista y nuevo dino

--- dinosaurios/models.py
+++ dinosaurios/models.py
@@ -14,8 +14,7 @@ class Dinosaurio(models.Model):
     altura = models.DecimalField(max_digits=5, decimal_places=2)
     periodo = models.ForeignKey(Periodo, verbose_name="Periodo", 
         on_delete=models.CASCADE)
-    imagen = models.ImageField("Imágen", upload_to=None, 
-        height_field=None, width_field=None, max_length=None)
+    imagen = models.ImageField("Imágen", upload_to='dinos')
 
     def __str__(self):
         return self.nombre
--- jurassic_park/settings.py
+++ jurassic_park/settings.py
@@ -61,6 +61,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -122,3 +123,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
