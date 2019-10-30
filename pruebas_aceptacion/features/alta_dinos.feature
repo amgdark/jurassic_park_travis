f41db84ed4fad47c7f439111d9de1a6121e3067e
Alex Mauricio <amgdark@uaz.edu.mx>
2019-10-30 13:49:54 -0600

    Agrega pruebas de aceptación para el feature alta dinosaurio

--- pruebas_aceptacion/features/alta_dinos.feature
+++ pruebas_aceptacion/features/alta_dinos.feature
@@ -5,6 +5,6 @@ Característica: Alta dinosaurio
     
     Escenario: Datos correctos para dinosaurio
         Dado Que ingreso al formulario para llenar los datos del dinosaurios
-        Y escribo los datos: nombre "t-rex", altura: "5", periodo: "cretacio"
+        Y escribo los datos: nombre "t-rex", altura: "5", periodo: "cretacio", imagen: "/Users/alexmau/Downloads/alex.png"
         Cuando presiono el botón agregar
         Entonces puedo ver el dinosario "t-rex" en la lista de dinosaurios
--- pruebas_aceptacion/features/steps/alta_dinos.py
+++ pruebas_aceptacion/features/steps/alta_dinos.py
@@ -1,21 +1,32 @@
 from behave import given, when, then
-
+from selenium import webdriver
+from unittest import TestCase
 
 @given(u'Que ingreso al formulario para llenar los datos del dinosaurios')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given Que ingreso al formulario para llenar los datos del dinosaurios')
+    driver = webdriver.Firefox()
+    driver.get('http://192.168.33.10:8000/periodo/nuevo/dino')
+    context.driver = driver
 
 
-@given(u'escribo los datos: nombre "t-rex", altura: "5", periodo: "cretacio"')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given escribo los datos: nombre "t-rex", altura: "5", periodo: "cretacio"')
+@given(u'escribo los datos: nombre "{nombre}", altura: "{altura}", periodo: "{periodo}", imagen: "{imagen}"')
+def step_impl(context,nombre, altura, periodo, imagen):
+    context.driver.find_element_by_id('id_nombre').send_keys(nombre)
+    context.driver.find_element_by_id('id_altura').send_keys(altura)
+    context.driver.find_element_by_id('id_periodo').send_keys(periodo)
+    context.driver.find_element_by_id('id_imagen').send_keys(imagen)
 
 
 @when(u'presiono el botón agregar')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When presiono el botón agregar')
+    context.driver.find_element_by_class_name('btn-primary').click()
+    
 
 
-@then(u'puedo ver el dinosario "t-rex" en la lista de dinosaurios')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then puedo ver el dinosario "t-rex" en la lista de dinosaurios')
+@then(u'puedo ver el dinosario "{dino}" en la lista de dinosaurios')
+def step_impl(context, dino):
+    rows =  context.driver.find_elements_by_tag_name('tr')
+    dinos = [row.find_elements_by_tag_name('td')[1].text for row in rows[1:] ]
+    
+    test = TestCase()
+    test.assertIn(dino,dinos)
