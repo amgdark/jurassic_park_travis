11705bc9be2d98377d50407bb1ba47bd4ecf29af
Alex Mauricio <amgdark@uaz.edu.mx>
2019-12-03 21:17:06 -0600

    Agrega pruebas unitarias para PDFs

--- dinosaurios/test/test_views.py
+++ dinosaurios/test/test_views.py
@@ -1,7 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from dinosaurios.models import Dinosaurio, Periodo, VotacionDino
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django_weasyprint import WeasyTemplateResponse
+from dinosaurios.views  import ListaDinoPdf, VistaPdf
 
 
 class TestViews(TestCase):
@@ -114,6 +116,67 @@ class TestViews(TestCase):
         estrellas += '<img src="/static/images/star-empty.svg" class="voto-img"></td>'
         response = self.client.get('/periodo/dinos/votacion')
         self.assertInHTML(estrellas,response.rendered_content)
+
+    def test_lista_pdf_status_code_200(self):
+        response = self.client.get('/periodo/dinos/lista_pdf')
+        self.assertEqual(response.status_code, 200)
+
+    def test_lista_pdf_template_used(self):
+        response = self.client.get('/periodo/dinos/lista_pdf')
+        self.assertTemplateUsed(response, 'dinosaurios/dinosaurio_pdf.html')
+
+    def test_lista_pdf_view_name(self):
+        response = self.client.get(reverse('lista_dinos_pdf'))
+        self.assertTemplateUsed(response, 'dinosaurios/dinosaurio_pdf.html')
+    
+    def test_lista_pdf_es_objeto_de_weasy_template_response(self):
+        response = self.client.get(reverse('lista_dinos_pdf'))
+        self.assertIsInstance(response, WeasyTemplateResponse)
+
+    def test_envio_datos_dino_pdf(self):
+        self.agrega_dino()
+        response = self.client.get('/periodo/dinos/lista_pdf')
+        self.assertIn('object_list', response.context)
+
+    def test_envio_t_rex_datos_dino_pdf(self):
+        self.agrega_dino()
+
+        response = self.client.get('/periodo/dinos/lista_pdf')
+        self.assertEquals('T-Rex', response.context['object_list'][0].nombre)
+
+    def test_lista_pdf_se_renderizo(self):
+        self.agrega_dino()
+        response = self.client.get(reverse('lista_dinos_pdf'))
+        self.assertTrue(response.is_rendered)
+    
+    def test_lista_pdf_content_type(self):
+        self.agrega_dino()
+        response = self.client.get(reverse('lista_dinos_pdf'))
+        self.assertIn("application/pdf", str(response))
+
+    def test_lista_pdf_nombre_archivo(self):
+        view_pdf = ListaDinoPdf()
+        self.assertEqual('lista_dinos.pdf', view_pdf.pdf_filename)
+
+    def test_lista_pdf_no_se_incluya_como_adjunto(self):
+        view_pdf = ListaDinoPdf()
+        self.assertFalse(view_pdf.pdf_attachment)
+
+    def test_lista_pdf_modelo_sea_dinosaurio(self):
+        view_pdf = ListaDinoPdf()
+        self.assertEqual(view_pdf.model, Dinosaurio)
+    
+    def test_lista_pdf_incluya_el_css_bootstrap(self):
+        view_pdf = ListaDinoPdf()
+        lista_css = view_pdf.pdf_stylesheets
+        bootstrap = lista_css[0].split('/')[-1]
+        self.assertEqual('bootstrap.min.css', bootstrap)
+
+    def test_lista_pdf_incluya_el_css_estilos(self):
+        view_pdf = ListaDinoPdf()
+        lista_css = view_pdf.pdf_stylesheets
+        estilos = lista_css[1].split('/')[-1]
+        self.assertEqual('estilos.css', estilos)
 
     def agrega_periodo(self):
         return Periodo.objects.create(nombre = 'Cretacio')
